e5587df1458afb983df16126fc546ab3273b9607
afr2903 <afr102903@gmail.com>
2024-03-05 00:08:24 -0600

    PID controller structure node created, and lauch file for Part 2

--- class_ws/src/motor_sim/scripts/pid_control.py
+++ class_ws/src/motor_sim/scripts/pid_control.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 
 import rospy
+
 #Declare libraries to be used (if needed)
+from std_msgs.msg import Float32
+import numpy as np
 #Declare Messages to be used
 
 
@@ -11,24 +14,62 @@ import rospy
 
 #Define other functions (if required)
 
+class Controller:
+    def __init__(self):
+        #Initialise and Setup node
+        rospy.init_node("Control")
+        self.loop_rate = rospy.Rate(200)
+        self.sample_time = rospy.get_param("~sample_time",0.01)
+
+        # Variables/Parameters to be used
+        self.kP = rospy.get_param("~kP",0.1)
+        self.kI = rospy.get_param("~kI",0.1)
+        self.kD = rospy.get_param("~kD",0.1)
+
+        self.setpoint = 0.0
+        self.feedback = 0.0
+        self.integral = 0.0
+        self.last_error = 0.0
+
+        self.last_time = rospy.Time.now().to_sec()
+
+        # Setup Publishers and Subscribers
+        self.sub_set_point = rospy.Subscriber('/set_point', Float32, self.set_point_callback)
+        
+        self.pub_motor_input = rospy.Publisher('/motor_input', Float32, queue_size=1)
+        self.sub_motor_output = rospy.Subscriber('/motor_output', Float32, self.motor_output_callback)
+
+    # Set Point Callback
+    def set_point_callback(self, msg):
+        self.setpoint = msg.data
+
+    # Motor output callback
+    def motor_output_callback(self, msg):
+        self.feedback = msg.data
+
+    def run(self):
+        dt = rospy.Time.now().to_sec() - self.last_time
+        if dt >= self.sample_time:
+            error = self.setpoint - self.feedback 
+            self.integral += error * dt
+            derivative = (error - self.last_error) / dt
+            out = self.kP * error + self.kI * self.integral + self.kD * derivative
+            out_msg = Float32()
+            out_msg.data = out
+            self.pub_motor_input.publish(out)
+
+            self.last_error = error
+            self.last_time = rospy.Time.now().to_sec()
+
+        self.loop_rate.sleep()
 
 if __name__=='__main__':
-    #Initialise and Setup node
-    rospy.init_node("Control")
-    rate = rospy.Rate(10)
-
-    #Declare other functions if required
-
-    #Setup Publishers and Subscribers
+    control = Controller()
 
     #Run the node
     try:
         while not rospy.is_shutdown():
-            
-            ######## Write your node here ##############
-
-            #Wait and repeat
-            rate.sleep()
+           control.run() 
             
     except rospy.ROSInterruptException:
         pass    
