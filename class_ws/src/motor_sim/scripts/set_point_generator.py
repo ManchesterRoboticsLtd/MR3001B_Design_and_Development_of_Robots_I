1034b09f46f2f41af11b9f13ff0896e1afe32f6c
afr2903 <afr102903@gmail.com>
2024-03-05 00:07:17 -0600

    Sinusoidal setpoint generator

--- class_ws/src/motor_sim/scripts/set_point_generator.py
+++ class_ws/src/motor_sim/scripts/set_point_generator.py
@@ -3,30 +3,38 @@
 import rospy
 
 #Declare libraries to be used
+import math
+from std_msgs.msg import Float32
 
 #Declare Messages to be used
 
 #Declare Variables/Parameters to be used
+signal = 0
 
-#Define callback functions (if required)
-
-#Define other functions (if required)
 
 if __name__=='__main__':
     #Initialise and Setup node
     rospy.init_node("SetPoint_Generator")
+    amplitude = rospy.get_param('~amplitude', 1.0)
+    frequency = rospy.get_param('~frequency', 1.0)
 
     # Configure the Node
-    rate = rospy.Rate(10)
+    rate = rospy.Rate(100)
 
     #Declare other functions if required
 
     #Setup Publishers and Subscribers
+    pub_setpoint = rospy.Publisher('/set_point', Float32, queue_size=10)
     
     try:
         while not rospy.is_shutdown():
             
             ######## Write your node here ##############
+            signal = amplitude * math.sin(2 * math.pi * frequency * rospy.Time.now().to_sec())
+
+            msg_setpoint = Float32()
+            msg_setpoint.data = signal
+            pub_setpoint.publish(msg_setpoint)
 
             rate.sleep()
 
